ce5187719813c8a78f552727f1efb14829e7d01d
kimengu-david <davidmutune200@gmail.com>
2021-07-29 20:55:24 +0000

    Add task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -7,10 +7,10 @@ CREATE PROCEDURE ComputeOverallScoreForUser(
 )
 BEGIN
    UPDATE users
-   SET overall_score = 
-   (SELECT AVG(score)
+   SET overall_score = (SELECT AVG(score)
     FROM corrections
 	WHERE corrections.user_id=user_id
 	GROUP BY corrections.user_id )
     WHERE id=user_id;
 END //
+DELIMITER;
