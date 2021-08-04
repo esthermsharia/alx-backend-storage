39ba31b206cf4206a3fa6458b61ea9f4340fe9e3
kimengu-david <davidmutune200@gmail.com>
2021-08-04 19:09:06 +0000

    Add return value type annotation for func replay

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -37,7 +37,7 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
-def replay(method: Callable):
+def replay(method: Callable) -> None:
     """displays the history of calls of a particular function."""
     redis = method.__self__._redis
     qualified_name = method.__qualname__
@@ -48,7 +48,6 @@ def replay(method: Callable):
     input_list = redis.lrange(input_key, 0, -1)
     output_list = redis.lrange(output_key, 0, -1)
     r_zipped = list(zip(input_list, output_list))
-
     for key, value in r_zipped:
         key = key.decode("utf-8")
         value = value.decode("utf-8")
