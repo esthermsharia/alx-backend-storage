6a03e90c9a1e3f24d182f4ef2f4972e02551837d
kimengu-david <davidmutune200@gmail.com>
2021-08-04 15:12:11 +0000

    Remove type annotation for return value

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: List[Union[str, bytes, int, float]]) -> str:
+    def store(self, data: List[Union[str, bytes, int, float]]):
         """Stores the data in redis"""
         r_key = str(uuid4())
         self._redis.set(r_key, data)
