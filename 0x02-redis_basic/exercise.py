1563621e9bdee0195c4e4d47a7249ee9f3e9e6e2
kimengu-david <davidmutune200@gmail.com>
2021-08-04 15:05:46 +0000

    redis-py store data in redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Defines a class that stores an instance of redis client
+   as a private variable and stores data in redis.
+"""
+import redis
+from uuid import uuid4
+from typing import Union, List
+
+
+class Cache:
+    def __init__(self):
+        """Instanciates a redis client"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: List[Union[str, bytes, int, float]]) -> str:
+        """Stores the data in redis"""
+        r_key = str(uuid4())
+        self._redis.set(r_key, data)
+        return r_key
