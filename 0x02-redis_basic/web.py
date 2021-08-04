00534e36f062ab8088c875f67ec68a400c7d08eb
kimengu-david <davidmutune200@gmail.com>
2021-08-04 19:37:41 +0000

    implement an expiring web cache and tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Defines a method that implements an expiring web cache and tracker."""
+
+import requests
+import redis
+
+
+def get_page(url: str) -> str:
+    """ obtains the HTML content of a particular URL and returns it."""
+    r = redis.Redis()
+    count = 0
+    r.set("cached:{}".format(url), count)
+    response = requests.get(url)
+    r.incr("count:{}".format(url))
+    r.setex("cached:{}".format(url), 10, r.get("cached:{}".format(url)))
+    return response
+
+
+if __name__ == "__main__":
+    get_page("http://slowwly.robertomurray.co.uk")
